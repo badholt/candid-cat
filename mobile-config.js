5dd58dae30fd03a6b337744bfad14b7481538286
Barbara Holt <worldwidebarbara@yahoo.com>
2016-02-29 02:58:11 -0500

    Devices Update 2.8.1 - comment out iOS device settings for android debugging - make all icon paths absolute to project root

--- mobile-config.js
+++ mobile-config.js
@@ -11,26 +11,26 @@ App.info({
 
 // Set up resources such as icons and launch screens.
 App.icons({
-    'android_ldpi': 'icons/inverted-logo.png',
-    'android_mdpi': 'icons/inverted-logo.png',
-    'android_hdpi': 'icons/inverted-logo.png',
-    'android_xhdpi': 'icons/inverted-logo.png',
-    'ipad': 'icons/inverted-logo.png',
-    'ipad_2x': 'icons/inverted-logo.png',
-    'iphone': 'icons/inverted-logo.png',
-    'iphone_2x': 'icons/inverted-logo@2x.png'
+    'android_ldpi': 'public/icons/inverted-logo.png',
+    'android_mdpi': 'public/icons/inverted-logo.png',
+    'android_hdpi': 'public/icons/inverted-logo.png',
+    'android_xhdpi': 'public/icons/inverted-logo.png',
+/*    'ipad': 'public/icons/inverted-logo.png',
+    'ipad_2x': 'public/icons/inverted-logo.png',
+    'iphone': 'public/icons/inverted-logo.png',
+    'iphone_2x': 'public/icons/inverted-logo@2x.png'*/
 });
 
 App.launchScreens({
-    'android_ldpi_landscape': 'icons/nine-logo.png',
-    'android_ldpi_portrait': 'icons/nine-logo.png',
-    'android_mdpi_landscape': 'icons/nine-logo.png',
-    'android_mdpi_portrait': 'icons/nine-logo.png',
-    'android_hdpi_landscape': 'icons/nine-logo.png',
-    'android_hdpi_portrait': 'icons/nine-logo.png',
-    'android_xhdpi_landscape': 'icons/nine-logo.png',
-    'android_xhdpi_portrait': 'icons/nine-logo.png',
-    'ipad_landscape': 'splash/Default~iphone.png',
+    'android_ldpi_landscape': 'public/icons/nine-logo.png',
+    'android_ldpi_portrait': 'public/icons/nine-logo.png',
+    'android_mdpi_landscape': 'public/icons/nine-logo.png',
+    'android_mdpi_portrait': 'public/icons/nine-logo.png',
+    'android_hdpi_landscape': 'public/icons/nine-logo.png',
+    'android_hdpi_portrait': 'public/icons/nine-logo.png',
+    'android_xhdpi_landscape': 'public/icons/nine-logo.png',
+    'android_xhdpi_portrait': 'public/icons/nine-logo.png',
+    /*'ipad_landscape': 'splash/Default~iphone.png',
     'ipad_portrait': 'splash/Default~iphone.png',
     'ipad_landscape_2x': 'splash/Default~iphone.png',
     'ipad_portrait_2x': 'splash/Default~iphone.png',
@@ -39,7 +39,7 @@ App.launchScreens({
     'iphone5': 'splash/Default@2x~iphone.png',
     'iphone6': 'splash/Default@2x~iphone.png',
     'iphone6p_landscape': 'splash/Default@2x~iphone.png',
-    'iphone6p_portrait': 'splash/Default@2x~iphone.png'
+    'iphone6p_portrait': 'splash/Default@2x~iphone.png'*/
 });
 
 // Set PhoneGap/Cordova preferences
